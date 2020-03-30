7ed6e3ba6b132cbd7a5c0909c534811afb4a3d63
Ana Nikolova <ani.nik19@gmail.com>
2020-03-30 12:58:59 +0200

    Dispay Experience & Education & Github Repos

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -5,6 +5,9 @@ import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
 import ProfileTop from "./ProfileTop";
 import ProfileAbout from "./ProfileAbout";
+import ProfileExperience from "./ProfileExperience";
+import ProfileEducation from "./ProfileEducation";
+import ProfileGithub from "./ProfileGithub";
 import { getProfileById } from "../../actions/profile";
 
 const Profile = ({
@@ -13,14 +16,15 @@ const Profile = ({
   auth,
   match
 }) => {
+  const nullProfile = !profile;
   useEffect(() => {
     getProfileById(match.params.id);
-  }, [getProfileById, match.params.id]);
+  }, [getProfileById, match.params.id, nullProfile]);
 
   return (
     <Fragment>
       {profile === null || loading ? (
-        <Spinner></Spinner>
+        <Spinner />
       ) : (
         <Fragment>
           <Link to="/profiles" className="btn btn-light">
@@ -30,12 +34,48 @@ const Profile = ({
             auth.loading === false &&
             auth.user._id === profile.user._id && (
               <Link to="/edit-profile" className="btn btn-dark">
-                Edit profile
+                Edit Profile
               </Link>
             )}
-          <div class="profile-grid my-1"></div>
-          <ProfileTop profile={profile} />
-          <ProfileAbout profile={profile} />
+          <div className="profile-grid my-1">
+            <ProfileTop profile={profile} />
+            <ProfileAbout profile={profile} />
+            <div className="profile-exp bg-white p-2">
+              <h2 className="text-primary">Experience</h2>
+              {profile.experience.length > 0 ? (
+                <Fragment>
+                  {profile.experience.map(experience => (
+                    <ProfileExperience
+                      key={experience._id}
+                      experience={experience}
+                    />
+                  ))}
+                </Fragment>
+              ) : (
+                <h4>No experience credentials</h4>
+              )}
+            </div>
+
+            <div className="profile-edu bg-white p-2">
+              <h2 className="text-primary">Education</h2>
+              {profile.education.length > 0 ? (
+                <Fragment>
+                  {profile.education.map(education => (
+                    <ProfileEducation
+                      key={education._id}
+                      education={education}
+                    />
+                  ))}
+                </Fragment>
+              ) : (
+                <h4>No education credentials</h4>
+              )}
+            </div>
+
+            {profile.githubusername && (
+              <ProfileGithub username={profile.githubusername} />
+            )}
+          </div>
         </Fragment>
       )}
     </Fragment>
